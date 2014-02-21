680c7bd43de7020b22da30d57dc1e1ff9212aa9f
Colton Cantrell <coltonpersonal@gmail.com>
2014-02-21 11:55:49 -0500

    Added User class, updated database

--- DAL/User/User.vb
+++ DAL/User/User.vb
@@ -1,3 +1,47 @@
 ﻿Public Class User
-
+    Public Property userID() As Integer
+        Get
+            Return m_userID
+        End Get
+        Set(value As Integer)
+            m_userID = Value
+        End Set
+    End Property
+    Private m_userID As Integer
+    Public Property firstName() As String
+        Get
+            Return m_firstName
+        End Get
+        Set(value As String)
+            m_firstName = Value
+        End Set
+    End Property
+    Private m_firstName As String
+    Public Property lastName() As String
+        Get
+            Return m_lastName
+        End Get
+        Set(value As String)
+            m_lastName = Value
+        End Set
+    End Property
+    Private m_lastName As String
+    Public Property email() As String
+        Get
+            Return m_email
+        End Get
+        Set(value As String)
+            m_email = Value
+        End Set
+    End Property
+    Private m_email As String
+    Private Property password() As String
+        Get
+            Return m_password
+        End Get
+        Set(value As String)
+            m_password = Value
+        End Set
+    End Property
+    Private m_password As String
 End Class
