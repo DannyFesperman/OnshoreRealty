1b708c5f4e120070674318123de3720efaf3db4e
Colton Cantrell <coltonpersonal@gmail.com>
2014-02-21 16:45:16 -0500

    Added properties to State class

--- DAL/State/State.vb
+++ DAL/State/State.vb
@@ -1,3 +1,21 @@
 ﻿Public Class State
+    Public Property stateID() As String
+        Get
+            Return m_stateID
+        End Get
+        Set(value As String)
+            m_stateID = value
+        End Set
+    End Property
+    Private m_stateID As String
+    Public Property description() As String
+        Get
+            Return m_description
+        End Get
+        Set(value As String)
+            m_description = value
+        End Set
+    End Property
+    Private m_description As String
 
 End Class
