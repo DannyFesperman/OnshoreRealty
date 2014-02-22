7eeb231ee877df3c579d14bb0f59f260561b5c56
Colton Cantrell <coltonpersonal@gmail.com>
2014-02-22 08:29:16 -0500

    added write method and imports to StatesDAO

--- DAL/State/StateDAO.vb
+++ DAL/State/StateDAO.vb
@@ -1,3 +1,20 @@
-﻿Public Class StateDAO
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Data.SqlClient
+
+Public Class StateDAO
+    Public Sub Write(statement As String, parameters As SqlParameter())
+        Using connection As New SqlConnection("Data Source=.\SQLEXPRESS;Initial Catalog=Onshore_Realty;Integrated Security=SSPI;")
+            connection.Open()
+            Using command As New SqlCommand(statement, connection)
+                command.CommandType = System.Data.CommandType.StoredProcedure
+                command.Parameters.AddRange(parameters)
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
 
 End Class
