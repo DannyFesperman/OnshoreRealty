afeb315f6f81fbb0925a2bfd7b8923efd3b12446
Colton Cantrell <coltonpersonal@gmail.com>
2014-02-24 16:16:58 -0500

    Added RealtorDAO class

---
+++ DAL/Realtor/RealtorDAO.vb
@@ -0,0 +1,11 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Data.SqlClient
+
+Public Class RealtorDAO
+    Inherits Write
+
+End Class
