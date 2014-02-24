ec41366e95c0e8401702dde22871ce7b09abd01b
Colton Cantrell <coltonpersonal@gmail.com>
2014-02-24 15:01:11 -0500

    Updated Write methods

--- DAL/State/StateDAO.vb
+++ DAL/State/StateDAO.vb
@@ -6,16 +6,7 @@ Imports System.Threading.Tasks
 Imports System.Data.SqlClient
 
 Public Class StateDAO
-    Public Sub Write(statement As String, parameters As SqlParameter())
-        Using connection As New SqlConnection("Data Source=.\SQLEXPRESS;Initial Catalog=Onshore_Realty;Integrated Security=SSPI;")
-            connection.Open()
-            Using command As New SqlCommand(statement, connection)
-                command.CommandType = System.Data.CommandType.StoredProcedure
-                command.Parameters.AddRange(parameters)
-                command.ExecuteNonQuery()
-            End Using
-        End Using
-    End Sub
+    Inherits Write
     Public Function ReadState(statement As String, parameters As SqlParameter()) As List(Of State)
         Using connection As New SqlConnection("Data Source=.\SQLEXPRESS;Initial Catalog=Onshore_Realty;Integrated Security=SSPI;")
             connection.Open()
