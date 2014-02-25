c99cf662b7ed049df90a1505a76a130b251a1ddd
Christopher Barratta <barrattac@hotmail.com>
2014-02-25 15:20:50 -0500

    Login Page

---
+++ OnshoreRealty/Login/Login.aspx.vb
@@ -0,0 +1,24 @@
+﻿Imports BLL
+
+Public Class Login
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Protected Sub Login_Click(sender As Object, ByVal e As System.EventArgs)
+        Dim log As LoginServices = New LoginServices()
+        Dim user = New UserFM()
+        user.email = email.Text
+        user.password = password.Text
+        If log.ValidCredentials(user) Then
+            Session("UserID") = log.GetUserID(user.email)
+            Response.Redirect("~/Search/Search.aspx")
+        End If
+    End Sub
+
+    Protected Sub NewUser_Click(sender As Object, e As EventArgs)
+        Response.Redirect("~/Register/Register.aspx")
+    End Sub
+End Class
