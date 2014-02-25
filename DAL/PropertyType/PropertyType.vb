0bc358077ac83f0bfc1432171710cc97b2e9489e
Colton Cantrell <coltonpersonal@gmail.com>
2014-02-25 13:18:01 -0500

    Added properties in StatusType

--- DAL/PropertyType/PropertyType.vb
+++ DAL/PropertyType/PropertyType.vb
@@ -1,12 +1,12 @@
 ﻿Public Class PropertyType
     Inherits BaseCodeTable
-    Public Property typeID() As String
+    Public Property typeID() As Integer
         Get
             Return m_typeID
         End Get
-        Set(value As String)
+        Set(value As Integer)
             m_typeID = value
         End Set
     End Property
-    Private m_typeID As String
+    Private m_typeID As Integer
 End Class
--- DAL/StatusType/StatusType.vb
+++ DAL/StatusType/StatusType.vb
@@ -1,3 +1,13 @@
 ﻿Public Class StatusType
+    Inherits BaseCodeTable
+    Public Property statusID() As Integer
+        Get
+            Return m_statusID
+        End Get
+        Set(value As Integer)
+            m_statusID = value
+        End Set
+    End Property
+    Private m_statusID As Integer
 
 End Class
